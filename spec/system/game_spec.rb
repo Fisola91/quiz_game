d45d183bde7d59a7f1ce7a2a5013cb3754979326
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-02-02 23:40:28 +0100

    A player can answer question correctly, generate another question and answer wrongly

--- spec/system/game_spec.rb
+++ spec/system/game_spec.rb
@@ -30,25 +30,61 @@ RSpec.describe "game" do
     expect(page).to have_text("You are welcome to quiz game")
     click_on "Sign out"
   end
+  context "when generates one question" do
+    it "allows a player to answer the question correctly" do
+      visit "/"
+      expect(page).to have_text("Quiz Game")
+      expect(page).to have_text("Enter your name to start the quiz")
+      fill_in "Player name", with: "Fisola"
+      click_on "Start player session"
+      expect(page).to have_text("You are playing as Fisola")
+      expect(page).to have_text("You are welcome to quiz game")
+      click_on "start quiz"
 
-  it "allows a player to answer the question correctly" do
-    visit "/"
-    expect(page).to have_text("Quiz Game")
-    expect(page).to have_text("Enter your name to start the quiz")
-    fill_in "Player name", with: "Fisola"
-    click_on "Start player session"
-    expect(page).to have_text("You are playing as Fisola")
-    expect(page).to have_text("You are welcome to quiz game")
-    click_on "start quiz"
+      game = JSON.parse(Game.last.question)
+      question = game.first
+      answer = game.last.join
+      response = game[1].first
 
-    game = JSON.parse(Game.last.question)
-    question = game.first
-    answer = game.last.join
-    response = game[1].first
+      choose(option: response)
+      click_on "Check answer"
 
-    choose(option: response)
-    click_on "Check answer"
+      expect(page).to have_text("You are correct!")
+    end
+  end
 
-    expect(page).to have_text("You are correct!")
+  context "when generates two questions" do
+    it "allows a player to answer questions correctly and wrongly, respectively" do
+      visit "/"
+      expect(page).to have_text("Quiz Game")
+      expect(page).to have_text("Enter your name to start the quiz")
+      fill_in "Player name", with: "Fisola"
+      click_on "Start player session"
+      expect(page).to have_text("You are playing as Fisola")
+      expect(page).to have_text("You are welcome to quiz game")
+      click_on "start quiz"
+
+      game = JSON.parse(Game.last.question)
+      question = game.first
+      answer = game.last.join
+      response = game[1].first
+
+      choose(option: response)
+      click_on "Check answer"
+
+      expect(page).to have_text("You are correct!")
+
+      click_on "Generate question"
+
+      game = JSON.parse(Game.last.question)
+      question = game.first
+      answer = game.last.join
+      response = game[1].last
+
+      choose(option: response)
+      click_on "Check answer"
+
+      expect(page).to have_text("You are wrong! The answer is #{answer.upcase}")
+    end
   end
 end
